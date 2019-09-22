fbc0687af68597a7c1fa28a6a05d5fceef63646c
Ozgur Ozer <ozgr@live.com>
2019-09-22 20:50:35 +0300

    Change name.

--- src/frontend/js/index.jsx
+++ src/frontend/js/index.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactDOM from 'react-dom'
 
 import './../css/style.scss'
 
-import Home from './Home'
+import { MainProvider, MainContext } from './MainContext'
 
 const App = () => {
+  const { setState } = useContext(MainContext)
+
+  const next = e => {
+    e.preventDefault()
+    setState({
+      connected: true
+    })
+  }
+
   return (
-    <Home />
+    <div id='app'>
+      <form>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Partner ID'
+            className='form-control form-control-lg'
+          />
+        </div>
+
+        <button
+          onClick={next}
+          className='btn btn-primary btn-lg btn-block'
+        >
+          Connect
+        </button>
+      </form>
+    </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Main = () => {
+  return (
+    <MainProvider>
+      <App />
+    </MainProvider>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
