93f9fc2608673415735de4155cebf9d6a3d13421
Ozgur Ozer <ozgr@live.com>
2019-09-23 23:10:38 +0300

    Add backend.

---
+++ src/backend/server.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const _localIp = require('ip')
+const _publicIp = require('public-ip')
+
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '..', 'frontend', 'html'))
+app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
+
+const server = app.listen(process.env.PORT || 0, () => {
+  console.log('Example app listening on port http://localhost:' + server.address().port)
+})
+
+app.post('/get-public-ip', async (req, res) => {
+  const publicIp = await _publicIp.v4()
+  const result = { publicIp }
+  res.json(result)
+})
+
+app.get('*', (req, res) => {
+  const localIp = _localIp.address()
+  const port = server.address().port
+
+  res.render('index', {
+    defaults: {
+      localIp,
+      port
+    }
+  })
+})
